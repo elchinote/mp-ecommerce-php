f0c0cb73eb697288950cae88a3c8afff4fe1ceb9
Carlos Castro <carlos.castro@baris.com.ar>
2020-04-27 03:14:47 -0300

    V1.3

--- webhook.php
+++ webhook.php
@@ -5,6 +5,9 @@ $datos = [
   'id' => $_GET['id'],
 ];
 
-echo print_r($datos);
-/* Guardamos la información en un archivo de registro */
-file_put_contents('registro.log', json_encode($datos) . PHP_EOL,FILE_APPEND);
+if($_GET['topic']=="payment" || $_GET['topic'=="merchant_order"]){
+    echo print_r($datos);
+
+    /* Guardamos la información en un archivo de registro */
+    file_put_contents('registro.log', json_encode($datos) . PHP_EOL,FILE_APPEND);
+}
