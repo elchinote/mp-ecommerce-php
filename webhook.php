9738ad713e772bb5472744caea61f056e933cccf
Carlos Castro <carlos.castro@baris.com.ar>
2020-04-27 04:35:34 -0300

    V1.5

--- webhook.php
+++ webhook.php
@@ -8,11 +8,23 @@ MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a
 $type = isset($_GET["type"]) ? $_GET["type"] : isset($_POST["type"]) ? $_POST["type"] : null;
 $id = isset($_GET["id"]) ? $_GET["id"] : isset($_POST["id"]) ? $_POST["id"] : null;
 
+$datos = [
+    'type' =>$_GET['type'],
+    'topic' => $_GET['topic'],
+    'id' => $_GET['id'],
+];
+  
+
 switch ($type) {
     case "payment":
         $payment = MercadoPago\Payment::find_by_id($id);
         if (!empty($payment)) {
             header("HTTP/1.1 200 OK");
+
+            /* Guardamos la información en un archivo de registro */
+            file_put_contents('registro.log', json_encode($datos) . PHP_EOL,FILE_APPEND);
+
+
         } else {
             header("HTTP/1.1 400 NOT_OK");
         }
